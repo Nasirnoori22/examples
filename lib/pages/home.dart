deaccebf03ebc31b8d5002defa87603778705e71
nasirahmadnoori <nasirahmad.noori222@gmail.com>
2021-11-22 06:11:30 +0000

    sliverappbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,10 +6,6 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
