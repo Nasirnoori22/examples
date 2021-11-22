18e977e6c38f81b4d66ad3e6c7e5e8e3098bb33c
nasirahmadnoori <nasirahmad.noori222@gmail.com>
2021-11-22 08:44:26 +0000

    sliver search bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -15,14 +17,30 @@ class Home extends StatelessWidget {
             floating: true,
             pinned: true,
             flexibleSpace:  FlexibleSpaceBar(
-              title: Container(
-                  child: Row(
-                    children: [
-                     Icon(Icons.arrow_back,),
-
-                    ],
-                ),
-              ),
+              title:TextField (  
+                      style: TextStyle( color: Colors.black,),  
+                    decoration: InputDecoration(
+                      fillColor: Colors.black,
+                      contentPadding:
+                      const EdgeInsets.only(left: 14.0, bottom: 0.0, top: 0.0),
+                       focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                      borderRadius: BorderRadius.circular(25.7),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                      ),  
+                      hintText: 'Search jobs',
+                      hintStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 13,
+                    ), 
+                    ),  
+                    ),
+              background: Image.network('https://ouch-cdn2.icons8.com/swz6i1TfzNagcn_fUFBqatZ4Y56kSn4vCiFobg1g3IA/rs:fit:196:196/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNDE5/Lzg3YTNlOTE3LWNl/ZjAtNDg5OC04NDhi/LWE4ODk5ODMwMWZl/ZC5zdmc.png')
             ),
             expandedHeight: 280,
             
@@ -34,7 +52,7 @@ class Home extends StatelessWidget {
                 margin: const EdgeInsets.all(15),
                 child: Container(
                   color: Colors.blue[100 * (index % 9 + 1)],
-                  height: 80,
+                  height: 200,
                   alignment: Alignment.center,
                   child: Text(
                     "Item $index",
