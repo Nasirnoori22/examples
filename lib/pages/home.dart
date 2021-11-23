0851ae43370ba5081079fc21bf59c42001edc83a
nasirahmadnoori <nasirahmad.noori222@gmail.com>
2021-11-23 09:30:59 +0000

    sliver list

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,61 +11,101 @@ class Home extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            backgroundColor: Colors.white,
+            backgroundColor: Color(0xffF6F6F6),
             elevation: 0,
             forceElevated: true,
             floating: true,
             pinned: true,
-            flexibleSpace:  FlexibleSpaceBar(
-              title:TextField (  
-                      style: TextStyle( color: Colors.black,),  
-                    decoration: InputDecoration(
-                      fillColor: Colors.black,
-                      contentPadding:
-                      const EdgeInsets.only(left: 14.0, bottom: 0.0, top: 0.0),
-                       focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                      borderRadius: BorderRadius.circular(25.7),
-                      ),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                        color: Colors.black,
-                      ),
-                      borderRadius: BorderRadius.circular(20),
-                      ),  
-                      hintText: 'Search jobs',
-                      hintStyle: TextStyle(
-                      color: Colors.black,
-                      fontSize: 13,
-                    ), 
-                    ),  
-                    ),
-              background: Image.network('https://ouch-cdn2.icons8.com/swz6i1TfzNagcn_fUFBqatZ4Y56kSn4vCiFobg1g3IA/rs:fit:196:196/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNDE5/Lzg3YTNlOTE3LWNl/ZjAtNDg5OC04NDhi/LWE4ODk5ODMwMWZl/ZC5zdmc.png')
-            ),
-            expandedHeight: 280,
+            leading: IconButton(onPressed: (){},icon: Icon(Icons.arrow_back), color: Colors.black,),
+            expandedHeight: 50,
             
           ),
          SliverList(
-          delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-              return Container(
-                margin: const EdgeInsets.all(15),
-                child: Container(
-                  color: Colors.blue[100 * (index % 9 + 1)],
-                  height: 200,
-                  alignment: Alignment.center,
-                  child: Text(
-                    "Item $index",
-                    style: const TextStyle(fontSize: 30),
+              delegate: SliverChildListDelegate(
+                [
+                 Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Column(
+                                children: [
+                                  Container(
+                                    margin: const EdgeInsets.only(left: 0.0, right: 35.0, top: 12),    
+                                    child:Text('Jobs.af', style: TextStyle( fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xFF312651) ),),
+                                  ),
+                                  Container(
+                                    margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 15),
+                                    child:  Text('30 job Opportunity'),
+                                  )
+                                ],
+                              )
+                            ], 
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 15),
+                            height: 50,
+                            width: 50,
+                             decoration: BoxDecoration(
+                               color:  Color(0xFFF97B5B),
+                                border: Border.all(
+                                color: Color(0xFFF97B5B),
+                                ),
+                                borderRadius: BorderRadius.all(Radius.circular(15))
+                                ),
+                            child:IconButton(onPressed: (){},icon: Icon(Icons.filter_alt_outlined ), color: Colors.white,),
+                          )
+                        ],
+                      ),
+                      Container(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20),
+                              height: 60,
+                              width: 120,
+                              decoration: BoxDecoration(
+                              color:  Color(0xff312651),
+                                border: Border.all(
+                              
+                                ),
+                                borderRadius: BorderRadius.all(Radius.circular(18))
+                                ),
+                                child: Container(
+                                  padding: EdgeInsets.all(2.0),
+                                  margin: const EdgeInsets.only(left: 13.0,  top: 19),
+                                  child: Text('Most Relevant', style: TextStyle(color: Colors.white),),
+                                )
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(left: .0, right: 20.0, top: 20),
+                              height: 60,
+                              width: 120,
+                              decoration: BoxDecoration(
+                              color: Color(0xffE5E5E5),
+                                border: Border.all(
+                                  color: Colors.white
+                                ),
+                                borderRadius: BorderRadius.all(Radius.circular(18))
+                                ),
+                                child: Container(
+                                  padding: EdgeInsets.all(2.0),
+                                  margin: const EdgeInsets.only(left: 13.0,  top: 19),
+                                  child: Text('Most Relevant', style: TextStyle(color: Colors.black),),
+                                )
+                            ),
+                            
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-                ),
-              );
-            },
-            childCount: 1000, // 1000 list items
-          ),
-        ),
+                ],
+              ),
+            ),
         ],
-      
       )
     );
   }
